f4fdf474d062dae70c88eaa3c7d45773ca7e5195
malay <tomalaypandey.tanu@gmail.com>
2019-11-02 21:13:59 +0530

    declared dataset set data and target

--- logReg.py
+++ logReg.py
@@ -1,14 +1,30 @@
 import numpy as np
 import pandas as pd
+import maths
 
 from sklearn.datasets import load_breast_cancer
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 
+def findMin(data):
+    return min(data)
+
 dataset = load_breast_cancer()
 
 x = dataset.data
 y = dataset.target
+
+class FeatureScaling:
+    def __init__(self, data, type='Standardization'):
+        if type=='Normalization':
+            return (data - data.min())/(data.max()-data.min())
+        elif type == 'Standardization':
+            return (data-data.mean())
+
+
+
+
+class LogisticRegression:
 
 
 
