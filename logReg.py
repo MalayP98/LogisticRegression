b6b3eefb0f83a5fc877f9af10cbdccdb5eea567f
malay <tomalaypandey.tanu@gmail.com>
2019-11-04 21:19:55 +0530

    LinearFunction-->W

--- logReg.py
+++ logReg.py
@@ -12,6 +12,7 @@ dataset = load_breast_cancer()
 x = dataset.data
 y = dataset.target
 
+
 class FeatureScaling:
     def __init__(self, data, type='Normalization'):
         self.data = data
@@ -22,21 +23,35 @@ class FeatureScaling:
         if self.type == 'Normalization':
             for i in range(0, self.data.shape[1]):
                 self.data[:, i] = (self.data[:, i] - min(self.data[:, i])) / (
-                            max(self.data[:, i]) - min(self.data[:, i]))
+                        max(self.data[:, i]) - min(self.data[:, i]))
             return self.data
 
         elif self.type == 'Standardization':
             for i in range(0, self.data.shape[1]):
-                self.data[:, i] = (self.data[:, i] - np.mean(self.data[:,i])) / (np.var(self.data[:, i]))
+                self.data[:, i] = (self.data[:, i] - np.mean(self.data[:, i])) / (np.var(self.data[:, i]))
             return self.data
 
         elif self.type == 'Mean_Normalization':
             for i in range(0, self.data.shape[1]):
-                self.data[:, i] = (self.data[:, i] - np.mean(self.data[:,i])) / (
-                            max(self.data[:, i]) - min(self.data[:, i]))
+                self.data[:, i] = (self.data[:, i] - np.mean(self.data[:, i])) / (
+                        max(self.data[:, i]) - min(self.data[:, i]))
             return self.data
 
 
 class LogisticRegression:
-    def __init__(self):
+    def __init__(self, data, learning_rate):
+        self.data = data
+        self.learning_rate = learning_rate
+        self.theta = np.random.rand(self.data.shape[1] + 1, 1)
+        self.data = np.c_[np.ones((self.data.shape[0], 1)), self.data]
+
+    def sigmoid(self):
         pass
+
+    def linear_function(self, instance):
+        self.output = np.dot(self.data[instance], self.theta)
+        return self.output
+
+
+
+
