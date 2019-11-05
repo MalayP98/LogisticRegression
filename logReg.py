5debe949f58a4d759c38de2d47d06ed108bb4e33
malay <tomalaypandey.tanu@gmail.com>
2019-11-05 19:44:29 +0530

    test1

--- logReg.py
+++ logReg.py
@@ -13,6 +13,18 @@ x = dataset.data
 y = dataset.target
 
 
+def train_test_split(input, target, ratio):
+    shuffle_indices = np.random.permutation(input.shape[0])
+    test_size = int(input.shape[0]*ratio)
+    train_indices = shuffle_indices[:test_size]
+    test_indices = shuffle_indices[test_size:]
+    xTrain = input[train_indices]
+    yTrain = target[train_indices]
+    xTest = input[test_indices]
+    yTest = target[test_indices]
+    return xTrain, yTrain, xTest, yTest
+
+
 class FeatureScaling:
     def __init__(self, data, type='Normalization'):
         self.data = data
@@ -56,7 +68,7 @@ class LogisticRegression:
         return self.output
 
     def train(self, target):
-        for i in range(0,50):
+        for i in range(0,1000):
             self.error = self.sigmoid(self.linear_function()) - target
             """print("sigmoid o/p is \n",self.sigmoid(self.linear_function()))"""
             self.gradient = (np.dot(self.error.T, self.data)).T
@@ -64,14 +76,33 @@ class LogisticRegression:
             """print("error \n", self.error)
             print("gradient \n", self.gradient)
             print("theta \n", self.theta)"""
+        print("FINAL")
+        print("error \n", self.error)
+        print("gradient \n", self.gradient)
+        print("theta \n", self.theta)
+
+    def predict(self, input):
+        print("Predicted values are...")
+        return self.sigmoid(np.dot(input, self.theta))
+        print(self.sigmoid(np.dot(input(), self.theta)))
 
 
 
-xTest = np.array([[0,0],[0,1],[1,0],[1,1]])
+"""xTest = np.array([[0,0],[0,1],[1,0],[1,1]])
 target = np.array([[0],[0],[0],[1]])
-obj = LogisticRegression(xTest,0.01)
+obj = LogisticRegression(xTest,0.1)
 obj.train(target)
+obj.predict(xTest)"""
 
+featureScaling = FeatureScaling(x, type = 'Normalization')
+x = featureScaling.Scaling()
+
+xTrain, yTrain, xTest, yTest = train_test_split(x,y,0.8)
+
+logistic = LogisticRegression(xTrain, 0.1)
+logistic.train(yTrain)
+
+pred = logistic.predict(xTest)
 
 
 
